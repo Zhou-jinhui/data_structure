8e0cac2000f545cd7c6207dc71b58d4275e04162
Cassiopeia <86bouledeneige@gmail.com>
2021-05-17 22:41:12 +0800

    fixed bug

--- array/binary_search.js
+++ array/binary_search.js
@@ -1,16 +1,19 @@
 function binarySearch(array, target) {
 	let lowBound = 0;
 	let upBound = array.length - 1;
-	
+	let midIndex;	
 	while (lowBound <= upBound) {
-		let midPoint = Math.floor((upBound - lowBound) / 2);
-		if (array[midPoint] === target) {
-			return midPoint;	
-		} else if (array[midPoint] < target){
-			lowBound = midPoint;
+		midIndex = Math.floor((upBound + lowBound) / 2);
+		if (array[midIndex] === target) {
+			return midIndex;	
+		} else if (array[midIndex] < target){
+			lowBound = midIndex + 1;
 		} else {
-			upBound = midPoint;
+			upBound = midIndex - 1;
 		}
 	}
 	return -1;
 }
+
+let arr = [1, 3, 5, 7, 9, 33, 66, 88, 100];
+console.log(binarySearch(arr, 34));
